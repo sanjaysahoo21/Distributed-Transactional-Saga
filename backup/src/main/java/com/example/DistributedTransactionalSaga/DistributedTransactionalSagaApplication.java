483e4d4489340c8a9f8618cc08cc6e4b78ddbfab
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2026-02-19 14:41:09 +0530

    created backup folder

---
+++ backup/src/main/java/com/example/DistributedTransactionalSaga/DistributedTransactionalSagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DistributedTransactionalSaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionalSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTransactionalSagaApplication.class, args);
+	}
+
+}
---
+++ backup/src/test/java/com/example/DistributedTransactionalSaga/DistributedTransactionalSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DistributedTransactionalSaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTransactionalSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
