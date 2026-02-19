6703e99704eff4dea122b012aadd102eae4c2545
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2026-02-19 15:44:53 +0530

    implemented orders service

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -2,7 +2,40 @@ package com.example.orderservice.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.orderservice.entity.Order;
+import com.example.orderservice.repository.OrderRepository;
+import com.example.orderservice.state.OrderState;
+import com.example.orderservice.state.OrderEvent;
+import org.springframework.statemachine.StateMachineFactory;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
 @Service
 public class OrderService {
+
+    public static final String ORDER_ID_HEADER = "order_id";
+
+    private final OrderRepository orderRepository;
+    private final StateMachineFactory<OrderState, OrderEvent> stateMachineFactory;
+
+    public OrderService(OrderRepository orderRepository, StateMachineFactory<OrderState, OrderEvent> stateMachineFactory) {
+        this.orderRepository = orderRepository;
+        this.stateMachineFactory = stateMachineFactory;
+    }
+
+    public void createOrder(Order order){
+        order.setStatus(OrderState.ORDER_CREATED);
+        Order saveOrder = orderRepository.save(order);
+
+        StateMachine<OrderState, OrderEvent> stateMachine = stateMachineFactory.getStateMachine(saveOrder.getId());
+        stateMachine.start();
+
+        Message<OrderEvent> message = MessageBuilder
+            .withPayload(OrderEvent.CREATE_ORDER)
+            .setHeader(ORDER_ID_HEADER, saveOrder.getId())
+            .build();
+        stateMachine.sendEvent(message);
+    }
 
 }
