345a4f86b19218dae03bfa170866d63e5a0ccce1
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2026-02-19 15:44:11 +0530

    implemented orders entity

---
+++ order-service/src/main/java/com/example/orderservice/entity/Order.java
@@ -0,0 +1,98 @@
+package com.example.orderservice.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GenerationType;
+
+import com.example.orderservice.state.OrderState;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private Long customerId;
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderState status;
+
+    public Order() {
+    }
+
+    public Order(UUID id, Long customerId, Long productId, Integer quantity, BigDecimal amount, OrderState status) {
+        this.id = id;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public OrderState getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderState status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", customerId=" + customerId + ", productId=" + productId + ", quantity=" + quantity
+                + ", amount=" + amount + ", status=" + status + "]";
+    }
+}
